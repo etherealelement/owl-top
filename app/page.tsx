ed7b89edaf24aa915d6c8a56ef95bc45057c72f6
ethereal element <vitalik.safonov@list.ru>
2023-07-10 13:45:09 +0300

    init: meta data

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
 import Image from 'next/image'
+import {Metadata} from "next";
+
+export async function generateMetadata():Promise<Metadata> {
+  return {
+    title: "Page 3",
+  }
+}
 
 export default function Home() {
   return (
